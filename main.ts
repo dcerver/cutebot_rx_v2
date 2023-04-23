23445c540640a4a639d49ead48139b9f30be4e45
dcerver <121509667+dcerver@users.noreply.github.com>
2023-04-23 17:42:31 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,11 @@ radio.onReceivedValue(function (name, value) {
     if (name == "Speed") {
         cuteBot.motors(value, value)
     } else if (name == "Yaw") {
-    	
+        if (value > 10) {
+            cuteBot.motors(value, 5)
+        } else if (value < 10) {
+            cuteBot.motors(5, Math.abs(value))
+        }
     }
 })
 radio.setGroup(1)
